1776ede18652c8848f919a477ca5ed42137ce4cd
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-10-17 17:03:01 +0800

    what-pkg+

--- libav_test.go
+++ libav_test.go
@@ -2,9 +2,13 @@ package golibav
 
 import (
 	"github.com/mgr9525/go-libav/avcodec"
+	"github.com/mgr9525/go-libav/avutil"
 	"testing"
 )
 
 func Test1(t *testing.T) {
 	avcodec.RegisterAll()
+
+	v1, v2, v3 := avutil.Version()
+	println(v1, v2, v3)
 }
